4dfae3648b6ec7c3fb4cec808e34d6f08e72937d
myrice <qqlddg@gmail.com>
2010-07-30 15:31:06 +0800

    Setup backup file's directories
    None

--- .emacs.d/ownSetup.el
+++ .emacs.d/ownSetup.el
@@ -19,3 +19,13 @@
 
 (setq default-frame-alist 
 '((height . 30) (width . 80) (menu-bar-lines . 20) (tool-bar-lines . 0))) 
+
+;;backup settings
+(setq
+    backup-by-copying t ; 自动备份
+    backup-directory-alist
+    '(("." . "~/.emacs.d/backups")) ; 自动备份在目录"~/.emacs.d/backups"下
+    delete-old-versions t ; 自动删除旧的备份文件
+    kept-new-versions 6 ; 保留最近的6个备份文件
+    kept-old-versions 2 ; 保留最早的2个备份文件
+    version-control t) ; 多次备份
